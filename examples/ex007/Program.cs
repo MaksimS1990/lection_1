2fc0e3fe678cc9d1180e685c6f3eb23230878946
MaksimS <sipatovGT@mail.ru>
2022-10-13 13:42:02 +0400

    ex007if_else

---
+++ examples/ex007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наташа")
+{
+    Console.WriteLine("Привет, сладкая!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
